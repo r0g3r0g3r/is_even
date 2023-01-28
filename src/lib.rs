9fc3b5138801c2e22a377e2e6139acff82d2d7d4
r0g3r0g3r <iagoroger1516@gmail.com>
2023-01-28 18:04:22 -0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub fn is_even(number: i64) -> bool {
+    if number % 2 == 0 {
+        true
+    } else {
+        false
+    }
+} 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(is_even(2), true);
+        assert_eq!(is_even(3), false);
+        assert_eq!(is_even(4), true);
+        assert_eq!(is_even(5), false);
+    }
+}
